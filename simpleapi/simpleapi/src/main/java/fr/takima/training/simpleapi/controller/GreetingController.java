bc8fdc0f96c6648587e17d226666b238ebf67073
loicndebi <113634924+loicndebi@users.noreply.github.com>
2025-05-02 17:16:52 +0200

    second check

--- simpleapi/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ simpleapi/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -1,18 +1,20 @@
 package fr.takima.training.simpleapi.controller;
 
 import org.springframework.web.bind.annotation.*;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@CrossOrigin
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/")
+    @GetMapping
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 
-    record Greeting(long id, String content) {}
+    record Greeting(long id, String content) { }
 }
